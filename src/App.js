d9b3b2e451580b27f6f91684b81e20e8e52f9db0
Mulan Zhao <melaniezhao999@gmail.com>
2019-02-28 20:33:22 -0800

    added list component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import Toolbar from './toolbar';
+import AwardList from './components/CategoryInput/AwardList.jsx';
 
 class App extends Component {
-  constructor(p) {
-    super(p);
-    this.state = {};
-  }
+  // constructor(p) {
+  //   super(p);
+  //   this.state = {};
+  // }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <Toolbar name="Trevor" />
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <AwardList listTitle="APIs" inputBackgroundText="+ add API"/>
       </div>
     );
   }
