f867d305a0492a5cff180178ad6b62ef796f7808
Julia Wang <juliawang@Julias-MacBook-Pro.local>
2019-03-01 17:24:42 -0800

    almost done

--- src/components/JudgeInfo/Judge.jsx
+++ src/components/JudgeInfo/Judge.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import './Judge.css';
 
 class Judge extends Component {
   render() {
     return (
-      <div className={this.props.color ? 'dark-color' : 'light-color'}>
+      <div className={this.props.color % 2 == 0 ? 'dark-color' : 'light-color'}>
         <div className="judge-name">{this.props.name}</div>
         <div className="judge-api">{this.props.api}</div>
       </div>
--- src/components/JudgeInfo/JudgeInfo.jsx
+++ src/components/JudgeInfo/JudgeInfo.jsx
@@ -15,7 +15,8 @@ class JudgeInfo extends Component {
       curr_name: '',
       names: [],
       selected: '',
-      info: []
+      info: [],
+      count: 0
     };
     this.handleName = this.handleName.bind(this);
     this.addInfo = this.addInfo.bind(this);
@@ -35,23 +36,14 @@ class JudgeInfo extends Component {
   addInfo() {
     if (this.state.curr_name !== '') {
       this.setState({
+        info: this.state.info.concat([
+          [this.state.curr_name, this.state.selected.label, this.state.count]
+        ]),
         curr_name: '',
-        info: this.state.info.push([
-          this.state.curr_name,
-          this.state.selected.label
-        ])
+        count: this.state.count + 1,
+        selected: ''
       });
-    }
-  }
-
-  createJudge() {
-    var i;
-    for (i = 0; i < info.length; i++) {
-      if (i % 2 == 0) {
-        <Judge name={this.curr_name} api={option.label} color={false} />;
-      } else {
-        <Judge name={this.curr_name} api={option.label} color={true} />;
-      }
+      console.log(this.state.info);
     }
   }
 
@@ -66,35 +58,49 @@ class JudgeInfo extends Component {
       <div className="judge-info-page">
         <p className="judge-info-title">Judge Information</p>
 
-        <div className="input-container">
-          <p className="input-name-header">Judge Name</p>
-          <input
-            className="input-name"
-            placeholder="Judge Name"
-            value={this.state.curr_name}
-            onChange={this.handleName}
-          />
-
-          <p className="input-api-header">Judge API</p>
-
-          <div className="dropdown">
-            <Dropdown
-              options={options}
-              onChange={this._onSelect}
-              value={defaultOption}
-              placeholder="Select an option"
+        <div className="input-list">
+          <div className="input-container">
+            <p className="input-name-header">Judge Name</p>
+            <input
+              className="input-name"
+              placeholder="Judge Name"
+              value={this.state.curr_name}
+              onChange={this.handleName}
             />
+
+            <p className="input-api-header">Judge API</p>
+
+            <div className="dropdown">
+              <Dropdown
+                options={options}
+                onChange={this._onSelect}
+                value={defaultOption}
+                placeholder="Select an option"
+              />
+            </div>
+
+            <button
+              className="submit-button"
+              type="button"
+              onClick={this.addInfo}
+            >
+              Submit
+            </button>
           </div>
 
-          <button
-            className="submit-button"
-            type="button"
-            onClick={this.addInfo}
-          >
-            Submit
-          </button>
-
-          <div className="table" />
+          <div className="judge-info-list-border">
+            <ul className="judge-info-list">
+              <p className="name-list-header">Name</p>
+              <p className="api-list-header">API</p>
+              {this.state.info.map((name, api) => (
+                <Judge
+                  name={name[0]}
+                  api={this.state.info[api][1]}
+                  color={this.state.info[api][2]}
+                />
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     );
