64c69cee4a1cf7923961de977a2d79bce532acad
Andrew Kou <andrewhkou@gmail.com>
2019-03-11 23:49:27 -0700

    styling

--- src/components/JudgeLogin/JudgeLogin.jsx
+++ src/components/JudgeLogin/JudgeLogin.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 
-class JudgeLogin extends component {
+class JudgeLogin extends Component {
 
     constructor(props) {
         super(props);
