ef69f110d022f38fd7b2fa01ebb02b74a53f758d
Andrew Kou <andrewhkou@gmail.com>
2019-03-11 22:40:40 -0700

    first commit lmao

---
+++ src/components/JudgeLogin/JudgeLogin.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+
+class JudgeLogin extends component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+        
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default JudgeLogin;
