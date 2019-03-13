39e4017b2fbbe44cc300eae8b0c143f9d1a6edb5
Francesca Song <francescajsong@gmail.com>
2019-03-13 00:36:58 -0700

    pr

--- src/components/ScoringOverview/ScoringOverview.jsx
+++ src/components/ScoringOverview/ScoringOverview.jsx
@@ -6,8 +6,57 @@ import Project from './Project.jsx'
 class ScoringOverview extends Component {
   constructor(props) {
     super(props);
-  }
 
+    this.state = {
+        renderedList: [],
+        scoredList: [],
+        unscoredList: [],
+        showUnscored: true
+    };
+
+
+    this.fetchUnscored = this.fetchUnscored.bind(this);
+    this.fetchScored = this.fetchScored.bind(this);
+    }
+
+    componentDidMount() {
+        let fetchedData = [["Chancellor", "42024", "5"], ["Christ", "31413", "4"], ["Oski", "01134", "4"], ["GoBears", "58008", ""]];
+        let scoredData = [];
+        let unscoredData = [];
+        for (let i = 0; i < fetchedData.length; i++) {
+            let score = fetchedData[i][2];
+            let component = [fetchedData[i]];
+            if (score == "") {
+                scoredData = scoredData.concat(component);
+            }
+            else {
+                unscoredData = unscoredData.concat(component);
+            }
+        }
+        console.log(unscoredData);
+        console.log(scoredData);
+
+        this.setState ({
+            unscoredList: unscoredData,
+            scoredList: scoredData,
+            renderedList: unscoredData
+        });
+    }
+
+
+    fetchUnscored() {
+        this.setState ({
+            renderedList: this.state.unscoredList,
+            showUnscored: true
+        });
+    }
+
+    fetchScored() {
+        this.setState ({
+            renderedList: this.state.scoredList,
+            showUnscored: false
+        });
+    }
 
 
     render() {
@@ -15,15 +64,15 @@ class ScoringOverview extends Component {
             <div className="scoring-view">
                 <div className="score-button-box">
                     <div className="score-button">
-                        <button className="unscored">Unscored</button>
-                        <button className="scored">  Scored  </button>
+                        <button className={this.state.showUnscored ? "unscored" : "scored"} onClick={this.fetchUnscored}>Unscored</button>
+                        <button className={this.state.showUnscored ? "scored" : "unscored"} onClick={this.fetchScored}>  Scored  </button>
                     </div>
                 </div>
-
-                <Project name="Chancellor" identification="42024" score="5"/>
-                <Project name="Christ" identification="31413" score="5"/>
-                <Project name="Oski" identification="01134" score="4"/>
-                <Project name="GoBears" identification="58008" score="5"/>
+            <ul>
+            {this.state.renderedList.map((item, index) => (
+                <Project key={index} name={item[0]} identification={item[1]} score={item[2]}/>
+            ))}
+            </ul>
             </div>
         );
     }
