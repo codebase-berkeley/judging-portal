966a28fe12a11053fb8e7792fe8d1c803bfd4cf2
Lawrence <lawrencejiang@berkeley.edu>
2019-03-15 15:01:06 -0700

    Updates

--- precache-manifest.aac4c23453ee55ee71166e8f4ec476c9.js
+++ precache-manifest.aac4c23453ee55ee71166e8f4ec476c9.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/judging-portal/static/js/runtime~main.4bf85146.js"
   },
   {
-    "revision": "1235a2257917b82c6095",
-    "url": "/judging-portal/static/js/main.1235a225.chunk.js"
+    "revision": "76998ef2137bcad49bae",
+    "url": "/judging-portal/static/js/main.76998ef2.chunk.js"
   },
   {
     "revision": "9f76ac4bb2ff0333d085",
     "url": "/judging-portal/static/js/1.9f76ac4b.chunk.js"
   },
   {
-    "revision": "1235a2257917b82c6095",
+    "revision": "76998ef2137bcad49bae",
     "url": "/judging-portal/static/css/main.205caba7.chunk.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/judging-portal/static/css/1.9c98d246.chunk.css"
   },
   {
-    "revision": "611e9a83f7857d225d4242152e105ba5",
+    "revision": "775840b99f883e90a2f2050d89f97848",
     "url": "/judging-portal/index.html"
   }
 ];
