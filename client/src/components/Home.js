e025fc0c0b660cd4a0a7c45ecad652d2bc31aa9a
Lawrence Jiang <lawrence@Lawrences-iMac.local>
2019-03-17 01:22:45 -0700

    deploy server

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 class Home extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      dummyAPIdata: []
+    };
     this.routeToOrganizer = this.routeToOrganizer.bind(this);
     this.routeToJudge = this.routeToJudge.bind(this);
   }
@@ -17,12 +20,23 @@ class Home extends Component {
     this.props.history.push(path);
   }
 
+  componentDidMount() {
+    this.getDummyAPIData();
+  }
+
+  getDummyAPIData = () => {
+    fetch('/api/dummy')
+      .then(res => res.json())
+      .then(dummyAPIdata => this.setState({ dummyAPIdata: dummyAPIdata }))
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Project Home</h1>
         <button onClick={this.routeToOrganizer}>ORGANIZER</button>
         <button onClick={this.routeToJudge}>JUDGE</button>
+        <div> {this.state.dummyAPIdata} </div>
       </div>
     );
   }
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Put all API endpoints under '/api'
+app.get('/api/dummy', (req, res) => {
+  const passwords = ['hello world', 'hello world 2']
+
+  // Return them as json
+  res.json(passwords);
+
+  console.log(`Sent info`);
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Password generator listening on ${port}`);
