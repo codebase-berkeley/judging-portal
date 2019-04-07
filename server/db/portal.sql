6c35cef516ddd790afea2807d162497cfd1f2d74
lajiang2 <lawrencejiang@berkeley.edu>
2019-04-07 14:53:50 -0700

    initialized judge and projects db

--- server/db/portal.sql
+++ server/db/portal.sql
@@ -8,6 +8,28 @@ WITH ENCRYPTED PASSWORD 'password';
 \c portal;
 CREATE TABLE projects
 (
-  id SERIAL PRIMARY KEY,
-  email VARCHAR(254),
+  id integer PRIMARY KEY,
+  name VARCHAR(254),
+  github VARCHAR(254),
+  categories VARCHAR(254)[]
 );
+
+CREATE TABLE judges
+(
+  name VARCHAR(254),
+  API VARCHAR(254),
+  projects integer[]
+);
+
+INSERT INTO projects VALUES (1, 'mentored', 'https://github.com/codebase-berkeley/judging-portal', '{"best team", "funnest team", "coolest team"}');
+INSERT INTO projects VALUES (2, 'calhacks', 'https://github.com/codebase-berkeley/', '{"biggest hackathon"}');
+
+INSERT INTO judges VALUES ('lawrence', 'codebaseAPI', '{1, 2}');
+INSERT INTO judges VALUES ('parth', 'codebaseAPI', '{2}');
+INSERT INTO judges VALUES ('calhacks', 'calhacksAPI', '{1}');
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO root;
+
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO root;
+
+GRANT ALL PRIVILEGES ON DATABASE portal TO root;
