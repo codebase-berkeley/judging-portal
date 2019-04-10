6f8155b32a784caa0f23865488ae9428fef0a265
lajiang2 <lawrencejiang@berkeley.edu>
2019-04-09 19:03:56 -0700

    changed judge db structure

--- server/db/portal.sql
+++ server/db/portal.sql
@@ -16,17 +16,17 @@ CREATE TABLE projects
 
 CREATE TABLE judges
 (
-  name VARCHAR(254),
+  name VARCHAR(254) PRIMARY KEY,
   API VARCHAR(254),
-  projects integer[]
+  projects VARCHAR[][]
 );
 
 INSERT INTO projects VALUES (1, 'mentored', 'https://github.com/codebase-berkeley/judging-portal', '{"best team", "funnest team", "coolest team"}');
 INSERT INTO projects VALUES (2, 'calhacks', 'https://github.com/codebase-berkeley/', '{"biggest hackathon"}');
 
-INSERT INTO judges VALUES ('lawrence', 'codebaseAPI', '{1, 2}');
-INSERT INTO judges VALUES ('parth', 'codebaseAPI', '{2}');
-INSERT INTO judges VALUES ('calhacks', 'calhacksAPI', '{1}');
+INSERT INTO judges VALUES ('lawrence', 'codebaseAPI', '{{1, ?}, {2, ?}}');
+INSERT INTO judges VALUES ('parth', 'codebaseAPI', '{{2, ?}}');
+INSERT INTO judges VALUES ('calhacks', 'calhacksAPI', '{{1, ?}}');
 
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO root;
 
