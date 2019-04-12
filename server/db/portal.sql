dc51021dd095b16c2d15ae08a6e877b1a2a81e52
Francesca Song <francescajsong@gmail.com>
2019-04-11 21:20:28 -0700

    project table not building

--- server/db/portal.sql
+++ server/db/portal.sql
@@ -6,20 +6,21 @@ CREATE USER root
 WITH ENCRYPTED PASSWORD 'password';
 
 \c portal;
+CREATE TABLE judges
+(
+  name VARCHAR(254),
+  API VARCHAR(254),
+  judgeId integer PRIMARY KEY,
+  score integer
+);
+
 CREATE TABLE projects
 (
   id integer PRIMARY KEY,
   name VARCHAR(254),
   github VARCHAR(254),
-  categories VARCHAR(254)[]
-);
-
-CREATE TABLE judges
-(
-  name VARCHAR(254),
-  API VARCHAR(254),
-  projectId integer,
-  score integer
+  categories VARCHAR(254)[],
+  judgeId REFERENCES judges(judgeId)
 );
 
 CREATE TABLE lists 
@@ -29,20 +30,18 @@ CREATE TABLE lists
   general VARCHAR(254)
 );
 
-INSERT INTO projects VALUES (1, 'mentored', 'https://github.com/codebase-berkeley/judging-portal', '{"best team", "funnest team", "coolest team"}');
-INSERT INTO projects VALUES (2, 'calhacks', 'https://github.com/codebase-berkeley/', '{"biggest hackathon"}');
+INSERT INTO judges VALUES ('lawrence', 'codebaseAPI', 10, -1);
+INSERT INTO judges VALUES ('parth', 'codebaseAPI', 20, -1);
+INSERT INTO judges VALUES ('calhacks', 'calhacksAPI', 30, -1);
 
-INSERT INTO judges VALUES ('lawrence', 'codebaseAPI', '1', '-1');
-INSERT INTO judges VALUES ('lawrence', 'codebaseAPI', '2', '-1');
-INSERT INTO judges VALUES ('parth', 'codebaseAPI', '2', '-1');
-INSERT INTO judges VALUES ('calhacks', 'calhacksAPI', '1', '-1');
+INSERT INTO projects VALUES (1, 'mentored', 'https://github.com/codebase-berkeley/judging-portal', '{"best team", "funnest team", "coolest team"}', 30);
+INSERT INTO projects VALUES (2, 'calhacks', 'https://github.com/codebase-berkeley/', '{"biggest hackathon"}', 20);
+INSERT INTO projects VALUES (3, 'atlassian', 'https://github.com/codebase-berkeley/', '{"lit team"}', 10);
 
 INSERT INTO lists (API) VALUES ('Google Vision');
 INSERT INTO lists (API) VALUES ('Uber Autopilot');
 INSERT INTO lists (API) VALUES ('API #23');
-
 INSERT INTO lists (fellowships) VALUES ('UCB Golden Bear Fellowship');
-
 INSERT INTO lists (general) VALUES ('worst project');
 INSERT INTO lists (general) VALUES ('most average project');
 
