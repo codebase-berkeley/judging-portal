18e18f0a99c99b47892d0ad738c241e795567249
Warren <warrenwwang@berkeley.edu>
2019-04-21 13:14:32 -0700

    lawrences changed in portal

--- server/db/portal.sql
+++ server/db/portal.sql
@@ -40,14 +40,13 @@ INSERT INTO judges VALUES (10, 'lawrence', 'codebaseAPI');
 INSERT INTO judges VALUES (20, 'parth', 'general categories');
 INSERT INTO judges VALUES (30, 'calhacks', 'calhacksAPI');
 
-INSERT INTO projects VALUES (1, 'mentored', 'https://github.com/codebase-berkeley/judging-portal', '{"General Category 1", "codebaseAPI"}');
-INSERT INTO projects VALUES (2, 'calhacks', 'https://github.com/codebase-berkeley/', '{"General Category 1", "General Category2", "calhacksAPI"}');
+INSERT INTO projects VALUES (1, 'mentored', 'https://github.com/codebase-berkeley/judging-portal', '{"GC: Web", "codebaseAPI"}');
+INSERT INTO projects VALUES (2, 'calhacks', 'https://github.com/codebase-berkeley/', '{"GC: Mobile", "GC: Web", "calhacksAPI"}');
 INSERT INTO projects VALUES (3, 'atlassian', 'https://github.com/codebase-berkeley/', '{"calhacksAPI"}');
 
 INSERT INTO apis VALUES ('codebaseAPI');
 INSERT INTO apis VALUES ('calhacksAPI');
-INSERT INTO apis VALUES ('General Category 1');
-INSERT INTO apis VALUES ('General Category 2');
+INSERT INTO apis VALUES ('General Category');
 
 INSERT INTO scores VALUES (30, 3, 'calhacksAPI', 5);
 
