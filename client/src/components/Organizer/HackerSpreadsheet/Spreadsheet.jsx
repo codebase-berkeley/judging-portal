24f6f58f37523b81210878cc5f5774cebaa0c350
Mulan Zhao <melaniezhao999@gmail.com>
2019-04-21 13:05:17 -0700

    hi

--- client/src/components/Organizer/HackerSpreadsheet/Spreadsheet.jsx
+++ client/src/components/Organizer/HackerSpreadsheet/Spreadsheet.jsx
@@ -6,10 +6,29 @@ class Spreadsheet extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            projects: [['SyncUp', 3, 250], ['Codeacademy', 2, 29], ['Github', 1, 50]]
+            projects: []
         };
         this.routeToPrev = this.routeToPrev.bind(this);
     }
+
+    async componentDidMount() {
+        this.pullProjectInfo().then(result => {
+            let i; 
+            let projs = [];
+            for (i = 0; i < result.length; i++) {
+                projs[i] = [result[i].name, result[i].wave, result[i].table_number];
+                console.log(result[i]);
+            }
+            this.setState({projects: projs}); 
+        });
+    }
+
+    async pullProjectInfo() {
+        const res = await fetch('/api/projects'); 
+        const resJson = res.json();
+        return resJson;
+    }
+
 
     routeToPrev() {
         const path = "/project-breakdown";
@@ -34,9 +53,11 @@ class Spreadsheet extends Component {
                     <div className="content-spreadsheet">
                         <div className="list-area">
                             <div className="entries-list">
-                                <div className="entries-list-name">PROJECT NAME</div>
-                                <div className="entries-list-wave">WAVE</div>
-                                <div className="entries-list-table">TABLE</div>
+                                <div className="spreadsheet-titles">
+                                    <div className="entries-list-name">PROJECT NAME</div>
+                                    <div className="entries-list-wave">WAVE</div>
+                                    <div className="entries-list-table">TABLE</div>
+                                </div>
                                 <div className="entry-element">{projectEntries}</div>
                             </div>
                         </div>
