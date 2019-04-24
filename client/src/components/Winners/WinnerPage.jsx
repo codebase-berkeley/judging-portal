06e179465ef8667a29f57f6f407f713ae17b0cb4
Warren <warrenwwang@berkeley.edu>
2019-04-24 15:20:10 -0700

    shitty shitty

--- client/src/components/Winners/WinnerPage.jsx
+++ client/src/components/Winners/WinnerPage.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import Collapsible from 'react-collapsible';
 import './WinnerPage.css';
-import Dropdown from 'react-dropdown';
 import ReactTable from "react-table";
+import ReactBasicTable from 'react-basic-table';
 
 
 class WinnerPage extends Component {
@@ -29,22 +29,49 @@ class WinnerPage extends Component {
     }
 
     render() {
+        const columns = [{
+            Header: 'Ranking',
+            accessor: 'judgename'
+        }, {
+            Header: 'Project Name',
+            accessor: 'projectname'
+        }, {
+            Header: 'Score',
+            accessor: 'score'
+        }]
+
+
         return (
-            <div className="page-background" id="JudgeInfo">
+            <div className="page-background" id="">
                 <div className="page-header">WINNERS</div>
                 <div className="content-background">
-                    {Object.values(this.state.winners).map((winnerJSON, index) => (
+                    {Object.values(this.state.winners).map((categoryWinners, index) => (
                         <div>
-                            <Collapsible openedClassName="category" className="category" trigger={Object.keys(this.state.winners)[index]} transitionTime={100}>
-                                <div className = "winner-list">
-                                <h1>Name Judge Score</h1>
-                                {winnerJSON.map((project, i) => (
+
+                            <Collapsible
+                                trigger={<div className="triggerHeader">
+                                    {Object.keys(this.state.winners)[index]}
+                                </div>}
+                                transitionTime={100}>
+
+                                <div className="winner-list">
+                                    <ReactTable
+                                        className="-striped -highlight"
+                                        data={categoryWinners}
+                                        style={{ height: "200px" }}
+                                        columns={columns}
+                                        defaultPageSize={5}
+                                        showPageSizeOptions={false}
+                                    />
+
+                                    {/* <p>Name Judge Score</p>
+                                {categoryWinners.map((project, i) => (
                                     <ul>
                                         <div>
-                                            {i + 1 + ": " + project["projectname"] + " - Judge: " + project["judgename"] + " - Score: " + project["score"]}
+                                            {i + 1 + ": " + project["projectname"] + " - " + project["judgename"] + " - " + project["score"]}
                                         </div>
                                     </ul>
-                                ))}
+                                ))} */}
                                 </div>
                             </Collapsible>
                         </div>
