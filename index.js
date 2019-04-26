758e4c4d6e0f2e472da602abb1041228259c4598
lajiang2 <lawrencejiang@berkeley.edu>
2019-04-26 16:19:28 -0700

    forgot paren

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const cors = require('cors');
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'client/build'));
-// app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // ########### DATAENTRY START ###########
 
@@ -354,6 +353,8 @@ app.get('/api/winners', async (req, res) => {
     console.log(error.stack);
   }
 });
+
+// app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 
 const port = process.env.PORT || 5000;
 app.listen(port);
