8c78d32466937cc4d375fbe8ae0904e1332393ff
lajiang2 <lawrencejiang@berkeley.edu>
2019-04-26 16:02:07 -0700

    connection string

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 
 require('dotenv').config();
 
-const connectionString = 'postgres://jmyweiavbyjzvf:d652c97c384bedf796b69f40891f6fb42f558455b72c8b11dbe0c8aa2700d07e@ec2-23-23-195-205.compute-1.amazonaws.com:5432/dad3s1n6f3mkt9'
+const connectionString = process.env.DATABASE_URL
 const pool = new Pool({
   connectionString: connectionString,
 })
