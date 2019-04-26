a04dc29eb55ce414b7ed843c308926b59bc3f142
lajiang2 <lawrencejiang@berkeley.edu>
2019-04-25 21:12:38 -0700

    connection string

--- db/index.js
+++ db/index.js
@@ -4,7 +4,10 @@ const { Pool } = require('pg');
 
 require('dotenv').config();
 
-const pool = new Pool();
+const connectionString = 'postgres://jmyweiavbyjzvf:d652c97c384bedf796b69f40891f6fb42f558455b72c8b11dbe0c8aa2700d07e@ec2-23-23-195-205.compute-1.amazonaws.com:5432/dad3s1n6f3mkt9'
+const pool = new Pool({
+  connectionString: connectionString,
+})
 
 module.exports = {
   query: (text, params, callback) => pool.query(text, params, callback)
